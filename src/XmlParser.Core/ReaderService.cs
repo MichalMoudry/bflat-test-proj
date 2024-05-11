7e3d9569da6e954c92cd296c5a084c2827e4d8ff
Michal Moudrý <moudry.michal@live.com>
2024-05-11 12:06:07 +0200

    Project start

---
+++ src/XmlParser.Core/ReaderService.cs
@@ -0,0 +1,14 @@
+﻿using ClosedXML.Excel;
+
+namespace XmlParser.Core;
+
+public sealed class ReaderService
+{
+    public void ReadWorksheets(string file)
+    {
+        using var workbook = new XLWorkbook(file);
+        foreach (var worksheet in workbook.Worksheets)
+        {
+        }
+    }
+}
